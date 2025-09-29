cd0d05bdf8976a7f9eb01489830a68e6f2fc703d
Markus <m.hoffmann65@gmx.de>
2025-09-29 13:44:29 +0200

    Icon-Verweise in index.html und vite.config.js angepasst, damit vercel --prod auch die icons ausliefert!

--- vite.config.js
+++ vite.config.js
@@ -15,10 +15,10 @@ export default defineConfig({
                 background_color: '#ffffff',
                 theme_color: '#317EFB',
                 icons: [
-                    { src: 'pwa-192x192.png',
+                    { src: 'icon-192x192.png',
                     sizes: '192x192',
                     type: 'image/png', },
-                    { src: 'pwa-512x512.png',
+                    { src: 'icon-512x512.png',
                     sizes: '512x512',
                     type: 'image/png', },], 
             },
