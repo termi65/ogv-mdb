6943be418321223d83122659d8918c80c4b84ed8
Markus <m.hoffmann65@gmx.de>
2026-05-05 14:29:49 +0200

    lokale Datenbank umbenannt in ogv_mdb

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,7 @@
 import Dexie from "dexie";
 
 // Achtung der Deckel wird mit einer KundenId aufgerufen, da sich der Deckel immer mit dem Kunden identifizieren lässt!
-export const db = new Dexie("myLocalDB");
+export const db = new Dexie("ogv_mdb");
     db.version(1).stores({
         getränke: "++id, bezeichnung, preis",
         kunden: "++id, name, vorname, geburtstag",
